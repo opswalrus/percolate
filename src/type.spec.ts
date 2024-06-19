037bbe70f0ff491165a813e1e0e820607298d3d1
David Ellis <david@conquerthelawn.com>
2024-06-19 14:03:19 -0500

    updating tests

--- src/type.spec.ts
+++ src/type.spec.ts
@@ -15,36 +15,47 @@ describe("V", () => {
     ]);
     const obj = { foo: "bar" };
 
-    expect(V(num).isA(Number)).toBeTrue();
     expect(V(num).isA(Array)).toBeFalse();
     expect(V(num).isA(Function)).toBeFalse();
     expect(V(num).isA(Map)).toBeFalse();
+    expect(V(num).isA(Number)).toBeTrue();
     expect(V(num).isA(Object)).toBeFalse();
+    expect(V(num).isA(String)).toBeFalse();
+
+    expect(V(str).isA(Array)).toBeFalse();
+    expect(V(str).isA(Function)).toBeFalse();
+    expect(V(str).isA(Map)).toBeFalse();
+    expect(V(str).isA(Number)).toBeFalse();
+    expect(V(str).isA(Object)).toBeFalse();
+    expect(V(str).isA(String)).toBeTrue();
 
     expect(V(arr).isA(Array)).toBeTrue();
     expect(V(arr).isA(Function)).toBeFalse();
     expect(V(arr).isA(Map)).toBeFalse();
+    expect(V(arr).isA(Number)).toBeFalse();
     expect(V(arr).isA(Object)).toBeTrue();
-
-    expect(V(arr).isA(Array)).toBeTrue();
-    expect(V(arr).isA(Function)).toBeFalse();
-    expect(V(arr).isA(Map)).toBeFalse();
-    expect(V(arr).isA(Object)).toBeTrue();
+    expect(V(arr).isA(String)).toBeFalse();
 
     expect(V(fun).isA(Array)).toBeFalse();
     expect(V(fun).isA(Function)).toBeTrue();
     expect(V(fun).isA(Map)).toBeFalse();
-    expect(V(fun).isA(Object)).toBeTrue();
+    expect(V(fun).isA(Number)).toBeFalse();
+    expect(V(arr).isA(Object)).toBeTrue();
+    expect(V(fun).isA(String)).toBeFalse();
 
     expect(V(map).isA(Array)).toBeFalse();
     expect(V(map).isA(Function)).toBeFalse();
     expect(V(map).isA(Map)).toBeTrue();
-    expect(V(map).isA(Object)).toBeTrue();
+    expect(V(map).isA(Number)).toBeFalse();
+    expect(V(arr).isA(Object)).toBeTrue();
+    expect(V(map).isA(String)).toBeFalse();
 
     expect(V(obj).isA(Array)).toBeFalse();
     expect(V(obj).isA(Function)).toBeFalse();
     expect(V(obj).isA(Map)).toBeFalse();
-    expect(V(obj).isA(Object)).toBeTrue();
+    expect(V(obj).isA(Number)).toBeFalse();
+    expect(V(arr).isA(Object)).toBeTrue();
+    expect(V(obj).isA(String)).toBeFalse();
   });
 
   it("indicates the kind of the value", () => {
