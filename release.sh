9e25f9644e54520a5526430b0949f0501dec4590
David Ellis <david@conquerthelawn.com>
2024-07-04 16:00:04 -0500

    adding comments to release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 npm run build
-npm run release -- --ci
+npm run release -- --ci # increment version numbers
 jsr publish --allow-slow-types
 npm publish --access public
 npm publish --access public
